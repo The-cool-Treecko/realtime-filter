7fed4003e85247eafb9936b0f69da39c5566b953
The cool Treecko <77108834+The-cool-Treecko@users.noreply.github.com>
2021-11-23 17:54:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function preload() {}
+var nose_x;
+var nose_y; 
+
+function preload() {
+    noseImg = loadImage("https://i.postimg.cc/ZKGH8VdL/580b57fbd9996e24bc43bed5.png")
+}
 
 function setup() {
     canvas = createCanvas(300,300);
@@ -18,13 +23,19 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = "+results[0].pose.nose.x);
-        console.log("nose y = "+results[0].pose.nose.y);
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x = "+nose_x);
+        console.log("nose y = "+nose_y);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    stroke("orange");
+    fill("red")
+    circle(nose_x, nose_y,20);
+    image(noseImg,nose_x-15, nose_y-15,30,30);
 }
 
 function saveImg() {
